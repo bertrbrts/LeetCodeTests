8ef8fd80d318923b3ca0520061d2c4d5e3ad25e0
Joseph Roberts <bertrbrts@gmail.com>
2022-04-23 18:51:30 -0400

    adding PalindromeNumber

---
+++ LeetCodeLibrary/PalindromeNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary
+{
+    /// <summary>
+    /// https://leetcode.com/problems/palindrome-number/
+    /// Given an integer x, return true if x is palindrome integer.
+    /// </summary>
+    public class PalindromeNumber
+    {
+        public static bool Execute(int x)
+        {
+            bool result = false;
+            int sign = Math.Sign(x);
+            if (sign == 0)
+            {
+                result = true;
+            }
+            else if (sign == 1)
+            {
+                int d = x;
+                var digits = new List<int>();
+                var reverse = new List<int>();
+                while (d != 0)
+                {
+                    int n = d % 10;
+                    digits.Add(n);
+                    reverse.Add(n);
+                    d /= 10;
+                }
+
+                digits.Reverse();
+
+                result = digits.SequenceEqual(reverse);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ UnitTests/PalindromeNumberTest.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeLibrary;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class PalindromeNumberTest
+    {
+        [Test, TestCaseSource("cases")]
+        public void Execute(int x, bool expectedResult)
+        {
+            var result = PalindromeNumber.Execute(x);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        private static IEnumerable<TestCaseData> cases()
+        {
+            int test1 = 121;
+            bool expectedResult1 = true;
+            yield return new TestCaseData(test1, expectedResult1);
+
+            int test2 = -121;
+            bool expectedResult2 = false;
+            yield return new TestCaseData(test2, expectedResult2);
+
+            int test3 = 10;
+            bool expectedResult3 = false;
+            yield return new TestCaseData(test3, expectedResult3);
+        }
+    }
+}
